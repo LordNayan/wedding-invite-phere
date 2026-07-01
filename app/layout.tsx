4324a5c61f9869434a5b6995fa7bf3971472d086
Nayan Lakhwani <nayan.lakhwani@convosight.com>
2026-07-01 13:10:10 +0530

    feat: init

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL(
-    process.env.NEXT_PUBLIC_SITE_URL ?? "https://wedding-invite-iqtn.vercel.app"
+    process.env.NEXT_PUBLIC_SITE_URL ?? "https://wedding-nayan-blue.vercel.app"
   ),
   title: siteConfig.meta.title,
   description: siteConfig.meta.description,
